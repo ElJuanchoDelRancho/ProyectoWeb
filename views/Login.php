60d5cdef5201972cb1e8f4f1a9de36803affa57b
Arturo <arturo.carrillo.solorzano@gmail.com>
2018-03-18 19:33:23 -0600

    ya esta juan
    hola

---
+++ views/Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/Login.css">
+</head>
+<body>
+	<?php include('navbar.php'); ?>
+  <div class="login">
+    <div class="login-triangle"></div>
+    
+    <h2 class="login-header">Login</h2>
+
+    <form class="login-container">
+      <p><input type="Nombre" class="cien" placeholder="Nombre"><br></p>
+      <p><input type="Password" class="cien" placeholder="Password"></p>
+      <p><input type="submit" class="cien" value="Siguiente"></p>
+    </form>
+  </div>
+  <?php include("footer.php"); ?>
+</body>
+</html>
